2cd26a3fbfb8b3f74c5a047bf7cafb024b677f3a
Gabriel Thunig <gabriel.thunig@web.de>
2015-11-16 15:48:37 +0100

    assert added to test

--- java/org/matsim/example/MyJunitTest.java
+++ java/org/matsim/example/MyJunitTest.java
@@ -2,11 +2,13 @@ package org.matsim.example;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 public class MyJunitTest {
 
 	@Test
 	public void test1() {
-		
+		Assert.assertTrue(true);
 	}
 	
 }
